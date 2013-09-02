31a8fe6e29813b318e7916dd0f1cfc471c548adc
Torchz <fan@torchz.net>
2013-09-02 20:56:01 +0800

    init the wejoin rails frame

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wejoin::Application.config.secret_key_base = 'fdd2b350034dff2616e3024861f44b71c6ef7960b092cd4da21fe4b34b1b85988e82fabf8c695be2c7f045fae0742654e07a70cf8109bd221b668e673f3d0983'
