66c24498073950e025436e7920b3dac2d01157c2
tcz001 <fan@torchz.net>
2013-10-09 20:38:41 +0800

    delete useless controllers

--- app/controllers/activity_controller.rb
+++
@@ -1,5 +0,0 @@
-class ActivityController < ApplicationController
-  def view
-    @activity = Activity.find(params[:id])
-  end
-end
--- test/controllers/join_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class JoinControllerTest < ActionController::TestCase
-  test "should get activity" do
-    get :activity
-    assert_response :success
-  end
-
-end
