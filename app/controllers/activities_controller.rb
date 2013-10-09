3b4e7940600cc37fa5abefba318ea7d88c7f6770
tcz001 <fan@torchz.net>
2013-10-09 22:44:21 +0800

    add create form and action for activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,19 @@ class ActivitiesController < ApplicationController
   def show
     @activity = Activity.find params[:id]
   end
-  def edit
+
+  def new
+  end
+
+  def create
+    @activity = Activity.new(post_params)
+    @activity.save
+    redirect_to @activity
+  end
+
+  private
+
+  def post_params
+    params.require(:Activity).permit(:title, :desc)
   end
 end
